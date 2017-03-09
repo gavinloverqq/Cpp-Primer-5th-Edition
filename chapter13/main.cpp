c2b5b95d47d6800549d05f0afd7d5dc689de44ec
kun wan <779483309@qq.com>
2017-03-09 17:03:21 +0800

    commit

--- chapter13/main.cpp
+++ chapter13/main.cpp
@@ -79,6 +79,54 @@ private:
     int a;
     string s;
 };
+
+class HasPtr{
+public:
+    HasPtr(const std::string& s = std::string()):ps(new std::string(s)),i(0){}
+    HasPtr(const HasPtr& c){
+//        ps = c.ps;
+        ps = new std::string(*c.ps);//拷贝ps指向的对象，而不是拷贝指针本身 ？？？这里不是很懂
+        i = c.i;
+    }
+//    !!! 赋值构造函数这么写为什么出错
+    HasPtr& operator = (const HasPtr& hp);
+    /*HasPtr& operator = (const HasPtr& hp){
+        ps = new std::string(*hp.ps);//拷贝ps指向的对象，而不是拷贝指针本身 ？？？这里不是很懂
+        i = hp.i;
+        return *this;
+    }*/
+    ~HasPtr(){
+        delete ps;
+    }
+
+private:
+    std::string* ps;
+    int i;
+};
+
+HasPtr& HasPtr::operator = (const HasPtr& hp){
+    auto newps = new std::string(*hp.ps);//拷贝ps指向的对象，而不是拷贝指针本身 ？？？这里不是很懂
+    delete ps;
+    ps = newps;
+    i = hp.i;
+    return *this;
+}
+
+
+struct X{
+    X(){cout << " constructor " << endl;}
+    X(const X&){cout << "copy constructor " << endl;}
+    X& operator = (const X& t){cout << " copy-assignment operator" << endl;
+        return *this;}
+    ~X(){cout << " destructor " << endl;}
+};
+void f1(X x){}
+void f2(X& x){}
+
+
+
+
+
 int main() {
 
 //    !!! 使用insert或push,容器会对元素拷贝初始化,使用emplace会u直接调用构造函数直接初始化
@@ -116,6 +164,42 @@ int main() {
     {
         C5 c5(2,"ss");
         c5 = C5(4,"xx");
+    }
+
+//    !!! 练习13.13 这个例子值得仔细看看
+    pIndexofTest(5);
+    {
+        cout << " 局部变量 " << endl;
+        X x;
+        cout << "  " << endl;
+
+        cout << " 非引用参数传递 " << endl;
+        f1(x);
+        cout << "  " << endl;
+
+        cout << " 引用参数传递 " << endl;
+        f2(x);
+        cout << "  " << endl;
+
+        cout << " 动态分配 " << endl;
+        X* px = new X;
+        cout << "  " << endl;
+
+        cout << " 添加到容器 " << endl;
+        vector <X> vX;
+        vX.push_back(x);
+        cout << "  " << endl;
+
+        cout << " 动态释放 " << endl;
+        delete px;
+        cout << "  " << endl;
+
+        cout << " 间接初始化和赋值 " << endl;
+        X y = x;
+        y = x;
+        cout << "  " << endl;
+
+        cout << " 程序结束 " << endl;
     }
     return 0;
 }
