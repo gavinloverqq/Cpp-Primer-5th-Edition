580c49a52d280e365b71953479aeb47bb52a9e71
macbookAir <779483309@qq.com>
2017-05-02 21:50:40 +0800

    mapped_type

--- chapter11/main.cpp
+++ chapter11/main.cpp
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <utility>
 
 using namespace std;
 //使用 ! 表示重要程度,! 越多越重要
@@ -105,6 +106,63 @@ int main() {
             cout << endl;
         }
     }
+
+//    pair
+    pIndexofTest(4);
+    {
+        string str[10] = {"asc", "sddf", "key", "word", "key", "words", "sss", "cc", "ssd", "dd"};
+        vector <pair<string, int>> pairVec;
+        for (int i = 0; i < sizeof(str) / sizeof(str[0]) ; ++i) {
+            pairVec.push_back(make_pair(str[i], i));
+        }
+        for (auto p : pairVec) {
+            cout << p.first << " " << p.second << endl;
+        }
+    }
+
+//    ! mapped_type 只有map type有
+    pIndexofTest(5);
+    {
+        set <string>::value_type v1;//v1 is a string
+        set <string>::key_type v2;//v2 is a string
+        map <string, int>::value_type v3;//v3 is a pair
+        map <string, int>::key_type v4;//v4 is a string
+        map <string, int>::mapped_type v5;//v5 is an int
+    }
+
+//    !! key is const, value is not
+    pIndexofTest(6);
+    {
+        set <string> exclude = {"key", "cc"};
+        map<string, size_t> word_count;
+        string str[10] = {"asc", "sddf", "key", "word", "key", "words", "sss", "cc", "ssd", "dd"};
+        for (int i = 0; i < sizeof(str) / sizeof(str[0]) ; ++i) {
+            if(exclude.find(str[i]) == exclude.end())
+                ++word_count[str[i]];
+        }
+        auto map_it = word_count.begin();
+        cout << map_it->first << " " << map_it->second << endl;
+//        map_it->first = "new key"; // key is const!     //   error: no viable overloaded '='
+        ++map_it->second;
+
+        map<string, size_t >::mapped_type v = map_it->second;
+        map<string, size_t >::value_type p = *(map_it++);
+        cout << v << endl;
+        cout << p.first << " " << p.second << endl;
+    }
+
+//    !! Iterators for sets are const
+    pIndexofTest(7);
+    {
+        set <int> iset = {0, 1, 2, 3, 4, 5};
+        set <int>::iterator set_it = iset.begin();
+        while (set_it != iset.end()){
+//            *set_it = 42; // error: cannot assign to return value because function 'operator*' returns a const value
+            cout << *set_it++ << " ";
+        }
+
+    }
+
 
     return 0;
 }
